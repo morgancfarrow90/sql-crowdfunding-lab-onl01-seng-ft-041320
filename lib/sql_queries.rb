1f5e326f837c98d6903265f73a701bc9338394f0
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-12 20:31:24 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, projects.pledge FROM projects
-  INNER JOIN projects ON pledges.project_id = 
+  INNER JOIN projects ON pledges.project_id = projects.id
   ORDER BY(projects.title)"
 end
 
