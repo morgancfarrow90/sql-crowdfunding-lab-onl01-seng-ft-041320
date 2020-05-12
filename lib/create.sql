302907c465d368b4e092e599df508045fd77cb7e
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-12 20:21:28 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,3 @@
-CREATE TABLE projects (id PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date)
+CREATE TABLE projects (id PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
+CREATE TABLE users (id PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE pledges (id PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
